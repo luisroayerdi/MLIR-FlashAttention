f2888458f497b12087ad1b171c2a033733e65b08
Luis Ayerdi-Morales <luisroayerdi@hotmail.com>
2026-04-23 21:13:02 -0500

    Update include/Attention/AttentionOps.h

--- include/Attention/AttentionOps.h
+++ include/Attention/AttentionOps.h
@@ -14,6 +14,7 @@
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/Interfaces/InferTypeOpInterface.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
+#include "mlir/Interfaces/DestinationStyleOpInterface.h"
 
 #define GET_OP_CLASSES
 #include "Attention/AttentionOps.h.inc"
